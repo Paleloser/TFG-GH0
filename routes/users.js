c96b71eff85524f04938320021b4453e2a1875c2
Pablo Caraballo Llorente <pablo.caraballorente@gmail.com>
2020-06-07 14:04:14 +0200

    feat(global): add authentication controller

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+const authController = require('../controllers/authentication');
+
+router.all('*', authController.auth);
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
