973c71c34b6d4f343ab9583e71faa931a83cd461
Pablo Caraballo Llorente <pablo.caraballorente@gmail.com>
2020-06-07 14:09:01 +0200

    Add authentication controller (#2)

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+const authController = require('../controllers/authentication');
+
+router.all('*', authController.auth);
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
